22223cfcaf0652ef0e75d49918339ea88ba8ad56
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2020-06-30 16:21:50 -0700

    auto-commit : Weekly Update at 16:21 on 30/06/2020

--- site_libs/commit.sh
+++ site_libs/commit.sh
@@ -2,7 +2,8 @@
 cd D:/Data-Insight
 git add --all
 timestamp() {
-  date +"on %d/%m/%Y"
+  date +"at %H:%M on %d/%m/%Y"
 }
 git commit -am "auto-commit : Weekly Update $(timestamp)"
+git pull
 git push origin master
